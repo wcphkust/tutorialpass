e5f20de8bc750641f5f0789d568f3b0d29755918
chasewang <cwangch@connect.ust.hk>
2020-06-06 21:20:56 +0800

    delete redundant files

--- VirtualFuncAnalysis/run.sh
+++
@@ -1,7 +0,0 @@
-export PATH=/path/to/llvm/bin:$PATH
-export LLVM_HOME=/path/to/llvm/home
-mkdir -p build && cd build && cmake .. && make && cd ..
-echo "=============Simple.cpp============="
-opt -load build/libVirtualCallAnalysisPass.so -virtual-call < target/simple.ll >> /dev/null
-echo "=============Hard.cpp============="
-opt -load build/libVirtualCallAnalysisPass.so -virtual-call < target/hard.ll >> /dev/null
